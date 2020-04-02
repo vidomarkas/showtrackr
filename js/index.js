395a7169c905f3231f63333fcfe4c9a91c736fd0
Viktoras Domarkas <vidomarkas@gmail.com>
2020-04-02 02:20:38 +0100

    start community section

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     centerPadding: "60px",
     variableWidth: true,
     slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1700,
     arrows: false,
     responsive: [
       {
@@ -28,4 +31,25 @@ $(document).ready(function() {
   });
 });
 
-console.log("hello");
+const tab1 = document.getElementById("tab1");
+const tab2 = document.getElementById("tab2");
+const tab3 = document.getElementById("tab3");
+
+tab1.addEventListener("click", () => {
+  tab1.classList.add("interfaces-tab", "interfaces-tab--active");
+  tab2.classList.remove("interfaces-tab--active");
+  tab3.classList.remove("interfaces-tab--active");
+  console.log("tab1");
+});
+tab2.addEventListener("click", () => {
+  tab2.classList.add("interfaces-tab", "interfaces-tab--active");
+  tab1.classList.remove("interfaces-tab--active");
+  tab3.classList.remove("interfaces-tab--active");
+  console.log("tab2");
+});
+tab3.addEventListener("click", () => {
+  tab3.classList.add("interfaces-tab", "interfaces-tab--active");
+  tab1.classList.remove("interfaces-tab--active");
+  tab2.classList.remove("interfaces-tab--active");
+  console.log("tab3");
+});
